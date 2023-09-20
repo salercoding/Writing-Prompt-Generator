b9a891cc91069ad2421a261be6820abf868e6c51
Selwan Al-Eryani <selwanaleryani@gmail.com>
2023-09-20 16:20:40 -0400

    moved arrays to a seperate file for easier readability, added button text to refresh prompt

--- src/Generator.js
+++ src/Generator.js
@@ -1,166 +1,28 @@
 import React, { useState } from "react";
+import { genreArray, settingArray, wordsArray, duplicates } from './Arrays';
 
 export default function Button() {
   //Random Index
   const randomIndex = (arr) => {
     return Math.floor(Math.random() * arr.length);
   };
-  //Genre
-  const genreArray = [
-    "Comedy",
-    "Drama",
-    "Thriller",
-    "Action",
-    "Mystery",
-    "Dramedy",
-    "Fantasy",
-    "Sci-Fi",
-    "Dystopian",
-    "Utopian",
-    "Historical",
-  ];
+
+  //states
+
+  const [buttonText, setButtonText] = useState("Generate a writing prompt!");
+
   const randomGenre = genreArray[randomIndex(genreArray)];
 
   const [genre, setGenre] = useState("Random Genre");
 
-  //Setting
-  const settingArray = [
-    "Abandoned Mine",
-    "Airplane",
-    "Airport Check-in",
-    "Alley",
-    "Amusement Park",
-    "Attic",
-    "Bakery",
-    "Bank",
-    "Basement",
-    "Barn",
-    "Beach",
-    "Birthday Party",
-    "Bonfire",
-    "Bowling Alley",
-    "Bridge",
-    "Bookstore",
-    "Cafeteria",
-    "Casino",
-    "Catacombs",
-    "Cave",
-    "City Park",
-    "Classroom",
-    "Closet",
-    "Coffee House",
-    "Courtroom",
-    "Cruise Ship",
-    "Cryogenic Sleep Chamber",
-    "Daycare",
-    "Desert",
-    "Diner",
-    "Dragon's Lair",
-    "Elevator",
-    "Farm",
-    "Forest",
-    "Frozen Tundra",
-    "Garage",
-    "Garden",
-    "Graveyard",
-    "Grocery Store",
-    "Halloween Party",
-    "Haunted House",
-    "Herbalist Shop",
-    "Hospital",
-    "Hotel Room",
-    "Kitchen",
-    "Laboratory",
-    "Lake",
-    "Library",
-    "Locker Room",
-    "Medieval Castle",
-    "Medieval Marketplace",
-    "Middle School Dance",
-    "Mountains",
-    "Movie Theatre",
-    "Night Club",
-    "Old Pick-Up Truck",
-    "Pirate Ship",
-    "Prison Cell",
-    "Public Pool",
-    "School Bus",
-    "Shopping Mall",
-    "Sleep-Away Camp",
-    "Spaceship",
-    "Subway Station",
-    "Swamp",
-    "Taxi cab",
-    "Treehouse",
-    "Busy Street",
-    "Arcade",
-    "Waiting Room",
-    "Theme Park",
-    "Wedding Ceremony",
-    "Zoo",
-  ];
   const randomSetting = settingArray[randomIndex(settingArray)];
 
   const [setting, setSetting] = useState("Random Setting");
 
-  //Three Words
-  const wordsArray = [
-    "Cake",
-    "Original",
-    "Button",
-    "Favor",
-    "Salmon",
-    "Earthwax",
-    "Compensation",
-    "Palm",
-    "Bother",
-    "Wolf",
-    "Prescription",
-    "Reader",
-    "Round",
-    "Bad",
-    "Consolidate",
-    "Negotiation",
-    "Banner",
-    "Musical",
-    "Manufacturer",
-    "Crystal",
-    "Thick",
-    "Record",
-    "Quest",
-    "Talk",
-    "Attraction",
-    "Dip",
-    "Wind",
-    "Demonstrate",
-    "Studio",
-    "Hospitality",
-    "Environment",
-    "Prisoner",
-    "Perfect",
-    "Move",
-    "Privacy",
-    "Prosecute",
-    "Provision",
-    "Mouse",
-    "Quality",
-    "Flash",
-    "Research",
-    "Horror",
-    "Dark",
-    "Aquarium",
-    "Pride",
-    "Random",
-    "Mercy",
-    "Photocopy",
-    "Consultation",
-    "Benefit",
-  ];
-
+ 
   //shuffle array to select random words
   let shuffledWords = wordsArray.sort(() => 0.5 - Math.random());
 
-  //set words
   const [firstWord, setFirstWord] = useState("Random Word");
   const [secondWord, setSecondWord] = useState("Random Word");
   const [thirdWord, setThirdWord] = useState("Random Word");
@@ -170,6 +32,7 @@ export default function Button() {
     <div>
       <h3
         onClick={() => {
+          setButtonText("Try another prompt!")
           setGenre(randomGenre);
           setSetting(randomSetting);
           setFirstWord(shuffledWords[0]);
@@ -177,8 +40,7 @@ export default function Button() {
           setThirdWord(shuffledWords[2]);
         }}
       >
-        {" "}
-        Click here!{" "}
+        {buttonText}
       </h3>
       <h4>Genre: {genre}</h4>
       <h4>Setting: {setting}</h4>
